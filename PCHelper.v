26cf077be82dbbfa2def1e7e4361196834b30782
Yujie Wang <yujie@charleswang.name>
2020-02-28 16:40:18 +0000

    fixed state_machine, pipelined JP instructions

--- PCHelper.v
+++ PCHelper.v
@@ -1,10 +1,12 @@
 module PCHelper(
 	input [11:0] norm,
 	input P,
-	input cnt_en, 
+	input cnt_en,
+	input JMP,
+	input [11:0] J,
 	output [11:0] op 
 );
 //	reg [11:0] one = 000000000001;
 //	assign op = (P & cnt_en) ? (pipe + one) : norm;
-	assign op = (P & cnt_en) ? (norm + 11'b000000000001) : norm;
+	assign op = JMP ? J : (P & cnt_en) ? (norm + 11'b000000000001) : norm;
 endmodule
