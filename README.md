33f3c37b154cdd9c45af9094e605515a90b2489f
Yujie Wang <yujie@charleswang.name>
2020-03-05 02:33:00 +0000

    trying to solve task 6

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DECA_5
 
-- [x] ##### Task 1. If you have been working separately from your lab partner choose whichever Lab4 design works best and copy this for both of you. Follow the instructions in Figure 1 to create a new Lab5 project which is a copy of the lab4 work and add the register file and ALU block to the Lab5 project.
+- [x] #### Task 1. If you have been working separately from your lab partner choose whichever Lab4 design works best and copy this for both of you. Follow the instructions in Figure 1 to create a new Lab5 project which is a copy of the lab4 work and add the register file and ALU block to the Lab5 project.
 
   1. In Quartus open your lab 4 project and archive it: *project* → *archive* will create a *.qar file from the entire project can be reconstructed.
 
@@ -36,7 +36,7 @@
 
      <img src="https://tva1.sinaimg.cn/large/0082zybpgy1gc8ew4ad6dj31e80u0wjw.jpg" alt="image-20200225015619760" style="zoom:25%;" />
 
-- [x] ##### Task 2. *Connect the inputs on the* *top* *block.* 
+- [x] #### Task 2. *Connect the inputs on the* *top* *block.* 
 
 - Connect EXEC1
 
@@ -44,7 +44,7 @@
 
   <img src="https://tva1.sinaimg.cn/large/0082zybpgy1gc8ev3vckwj31e80u0tj6.jpg" alt="image-20200225015507794" style="zoom:25%;" />
 
-- [x] ##### Task 3. Implement a boolean expression in the Verilog ALU to control write enable of the new registers wenout when used by ARMish instructions. The registers are written in the EXEC1 cycle of every ARMish instruction - but must NOT be written during execution of normal MU0 instructions. Note that there is a separate enable r0wen that is used by the MU0 instructions to write to R0 (which takes the place of Acc).
+- [x] #### Task 3. Implement a boolean expression in the Verilog ALU to control write enable of the new registers wenout when used by ARMish instructions. The registers are written in the EXEC1 cycle of every ARMish instruction - but must NOT be written during execution of normal MU0 instructions. Note that there is a separate enable r0wen that is used by the MU0 instructions to write to R0 (which takes the place of Acc).
 
   ```verilog
 assign wenout = exec1 & (&code);
@@ -54,7 +54,7 @@ assign wenout = exec1 & (&code);
 
   ![Rc7zEL](https://cdn.jsdelivr.net/gh/Ouikujie/image@master/Mac/Rc7zEL.png)
 
-- [x] ##### Task 4. Use the data path test program from Lab 4 to check that your MU0 instructions still work with the register file taking the place of Acc.
+- [x] #### Task 4. Use the data path test program from Lab 4 to check that your MU0 instructions still work with the register file taking the place of Acc.
 
   By comparing its IR/ACC/PC outputs with those from visual2deca, the CPU seems to be fine though a few problems were found. 
 
@@ -62,8 +62,81 @@ assign wenout = exec1 & (&code);
   
   <img src="https://tva1.sinaimg.cn/large/0082zybpgy1gc8ez7efmjj31760u0x2w.jpg" alt="Screenshot 2020-02-25 at 01.59.08" style="zoom:33%;" />
 
-- [ ] ##### Task 5. *Note Figure* *8* which is a quick reference allowing you to compose ARMish instructions in hex easily. Test your new ALU instructions, without Carry, using the text code provided.
+- [x] #### Task 5. Note Figure *8* which is a quick reference allowing you to compose ARMish instructions in hex easily. Test your new ALU instructions, without Carry, using the text code provided.
+
+  ```mif
+  // mif content
+  0 : 8123;
+  1 : C028;
+  2 : 8111;
+  3 : C02C;
+  4 : C00E;
+  5 : 7000;
+  ```
 
   
 
-- [ ] ##### Task 6. Implement a boolean expression in the Verilog ALU to control write enable of the new registers wenout when used by ARMish instructions. The registers are written in the EXEC1 cycle of every ARMish instruction.
+  ```verilog
+  // whether is ARM instruction
+  wire ARM = IR[15] & IR[14];
+   // ARMish opcode
+  assign arm_ADD = ARM & !IR[6] & !IR[5] & !IR[4];
+  assign arm_SUB = ARM & !IR[6] & !IR[5] & IR[4];
+  assign arm_MOV = ARM & !IR[6] & IR[5] & !IR[4];
+  assign arm_XSR = ARM & !IR[6] & IR[5] & IR[4];
+  
+  // control for increment in PC value 
+  assign PC_cnt_en = LDA & EXEC2 | STA & EXEC1 | ADD & EXEC2 | SUB & EXEC2 | JMI & EXEC1 & !MI | JEQ & EXEC1 & !EQ | LDI & EXEC1 | LSR & EXEC1 | ASR & EXEC1 | arm_ADD & EXEC1 | arm_SUB & EXEC1 | arm_MOV & EXEC1 | arm_XSR & EXEC1;
+  ```
+
+  ##### The new ARMish XSR instruction, when used with Rd = Rs = R0, can be used to replace MU0 LSR. Using Figure *7* *and* *9* determine the hex value of the ARMish instruction that will do this?
+
+  According to the instruction: 
+
+  > XSR shifts right (by 1), with bit 0 written into CARRY if S = 1, and cin shifted into bit 15. This combines ARM LSR, ASR, and RRX functionality.
+
+  `LSR` inserts 0 to the `MSB`, so `cin` is 0. 
+
+  ```mif
+  1100 | 0000 | 0101 | 0000
+  XSR R0,R0 => C030
+  ```
+
+  
+
+- [ ] #### Task 6.Implement in the Verilog ALU the boolean expressions required to write CARRY as specified by the *S* bit in Figure 7. The necessary signals are:
+
+  - [x] **cin**. The carry in to the ALU adder from **CIN** field.
+  
+    ```verilog
+    assign cin = cininstr[1] ? (cininstr[0] ? rsdata[15] : carryout) : (cininstr[0] ? 1 : 0);
+    ```
+  
+  - [x] **carryen**. Controls writing **CARRY**, derived from **S**, the ARMish opcode bits, and timing info from **EXEC1**.
+  
+    ```verilog
+    assign carryen = exec1 & alucout & cwinstr & (&code);
+    // alucout = alusum[16] => carry out from adder
+    // exec1 => timing
+    // cwinstr => S whether write CARRY
+    // code => ARMish Opcode (&code) both bits are high
+    ```
+  
+  - [x] **shiftin**. This determines the MSB of the result in the special case of **XSR**, as in Figure 7. Note that **shiftin** is just **cin** for the specified instructions.
+  
+    ```verilog
+    assign shiftin = cin;
+    ```
+  
+  - [x] **carryout**. To make **CARRY** work properly in right shifts (**XSR**) carryout must equal **rsdata[0]** in **XSR** and ALU cout otherwise. You may wish to use Verilog conditional operator to simplify this: condition ? thenpart : elsepart.
+  
+    ```verilog
+    wire XSR = !opinstr[2] & opinstr[1] & opinstr[0];
+    assign carryout	 = XSR ? rsdata[0] : alucout; 
+    // if OP=011, then rsdata[0], else alucout
+    ```
+  
+    
+  
+  ![WUxZGR](https://cdn.jsdelivr.net/gh/Ouikujie/image@master/Mac/WUxZGR.png)
+
--- alu.v
+++ alu.v
@@ -1,55 +1,68 @@
-module alu ( instruction, rddata, rsdata, carrystatus, skipstatus, exec1,
-    aluout, carryout, skipout, carryen, skipen, wenout) ;
-             
-input [15:0] instruction; // from IR'
-input exec1;          // timing signal: when things happen
-input [15:0] rddata;  // Rd register data outputs
-input [15:0] rsdata;  // Rs register data outputs
-input carrystatus;    // the Q output from CARRY
-inout skipstatus;     // the Q output from SKIP
-
-output [15:0] aluout;	// the ALU block output, written into Rd
-output carryout;	// the CARRY out, D for CARRY flip flop
-output skipout;        	// the SKIP output, D for SKIP flip flop
-output carryen;        	// the enable signal for CARRY flip-flop
-output skipen;         	// the enable signal for SKIP flip-flop
-output wenout;         	// the enable for writing Rd in the register file
-
-// these wires are for convenience to make logic easier to see
-wire [2:0] opinstr 	= instruction [6:4];	// OP field from IR'
-wire cwinstr 		= instruction [7];	// 1 => write CARRY: CW from IR'
-wire [3:0] condinstr 	= instruction [11:8];	// COND field from IR'
-wire [1:0] cininstr 	= instruction [13:12];	// CIN field from IR'
-wire [1:0] code 	= instruction [15:14];	// bits from IR': must be 11 for ARM instruction
-
-reg [16:0] alusum; 			// the 17 bit sum, 1 extra bit so ALU carry out can be extracted
-wire cin; 				// The ALU carry input, determined from instruction as in ISA spec
-wire shiftin; 				// value shifted into bit 15 on XSR, determined as in ISA spec
-
-assign alucout 	= alusum [16]; 		// carry bit from sum, or shift if OP = 011
-assign aluout 	= alusum [15:0]; 	// 16 normal bits from sum
-
-assign wenout	= exec1 & (&code);	// correct timing, to do: add enable condition
-assign carryen	= exec1 & alucout;	// correct timing, to do: add enable condition
-assign carryout	= alucout;		// this is correct
-                           		// note the special case of rsdata[0] when OP=011
-assign cin	= 0;   		     	// dummy, to do: replace with correct logic
-assign shiftin 	= 0;     		// dummy, to do: replace with correct logic
-
-assign skipout 	= 0;     		// dummy, to do: replace with correct logic
-assign skipen 	= exec1;  		// correct timing, to do: add enable condition
-
-always @(*) // do not change this line - it makes sure we have combinational logic
-  begin
-    case (opinstr)
-      3'b000  : alusum = rddata + rsdata + cin;			// if OP = 000
-      3'b001  : alusum = rddata + ~rsdata + cin;		// if OP = 001
-      3'b010  : alusum = rsdata + cin;				// if OP = 010
-      3'b011  : alusum = {rsdata[0], shiftin, rsdata[15:1]};	// if OP = 011
-      // to do (optional): add additional instructions as cases here
-      // available cases: 3'b100,3'b101,3'b110, 3'b111
-      default : alusum = 0;// default output for unimplemented OP values, do not change
-    endcase;
-  end
-
-endmodule
+module alu (instruction,
+            rddata,
+            rsdata,
+            carrystatus,
+            skipstatus,
+            exec1,
+            aluout,
+            carryout,
+            skipout,
+            carryen,
+            skipen,
+            wenout);
+    
+    input [15:0] instruction; // from IR'
+    input exec1;              // timing signal: when things happen
+    input [15:0] rddata;      // Rd register data outputs
+    input [15:0] rsdata;      // Rs register data outputs
+    input carrystatus;        // the Q output from CARRY
+    inout skipstatus;         // the Q output from SKIP
+    
+    output [15:0] aluout;	  // the ALU block output, written into Rd
+    output carryout;	      // the CARRY out, D for CARRY flip flop
+    output skipout;        	// the SKIP output, D for SKIP flip flop
+    output carryen;        	// the enable signal for CARRY flip-flop
+    output skipen;         	// the enable signal for SKIP flip-flop
+    output wenout;         	// the enable for writing Rd in the register file
+    
+    // these wires are for convenience to make logic easier to see
+    wire [2:0] opinstr 	 = instruction [6:4];	// OP field from IR'
+    wire cwinstr 		 = instruction [7];	// 1 = > write CARRY: CW from IR'
+    wire [3:0] condinstr 	 = instruction [11:8];	// COND field from IR'
+    wire [1:0] cininstr 	 = instruction [13:12];	// CIN field from IR'
+    wire [1:0] code 	 = instruction [15:14];	// bits from IR': must be 11 for ARM instruction
+    
+    wire arm = &code;
+    wire XSR = !opinstr[2] & opinstr[1] & opinstr[0];
+    
+    reg [16:0] alusum; 			// the 17 bit sum, 1 extra bit so ALU carry out can be extracted
+    wire cin; 				// The ALU carry input, determined from instruction as in ISA spec
+    wire shiftin; 				// value shifted into bit 15 on XSR, determined as in ISA spec
+    
+    assign alucout 	 = alusum [16]; 		// carry bit from sum, or shift if OP = 011
+    assign aluout 	 = alusum [15:0]; 	// 16 normal bits from sum
+    
+    assign wenout	 = exec1 & (arm);	// correct timing, to do: add enable condition
+    assign carryen	 = exec1 & alucout & cwinstr & (arm);	// correct timing, to do: add enable condition
+    assign carryout	 = XSR ? rsdata[0] : alucout;		// this is correct
+    // note the special case of rsdata[0] when OP = 011
+    assign cin	 = cininstr[1] ? (cininstr[0] ? rsdata[15] : carryout) : (cininstr[0] ? 1 : 0);   		     	//* DONE dummy, to do: replace with correct logic
+    assign shiftin 	 = cin;     		// dummy, to do: replace with correct logic
+    
+    assign skipout 	 = 0;     		// dummy, to do: replace with correct logic
+    assign skipen 	 = exec1;  		// correct timing, to do: add enable condition
+    
+    always @(*) // do not change this line - it makes sure we have combinational logic
+    begin
+        case (opinstr)
+            3'b000  : alusum = rddata + rsdata + cin;			// if OP = 000
+            3'b001  : alusum = rddata + ~rsdata + cin;		// if OP = 001
+            3'b010  : alusum = rsdata + cin;				// if OP = 010
+            3'b011  : alusum = {rsdata[0], shiftin, rsdata[15:1]};	// if OP = 011
+            // to do (optional): add additional instructions as cases here
+            // available cases: 3'b100,3'b101,3'b110, 3'b111
+            default : alusum = 0;// default output for unimplemented OP values, do not change
+            endcase;
+        end
+        
+        endmodule
