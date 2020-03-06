2fbf0305f812079fd8aa744737c3531d976cf9e7
Yujie Wang <yujie@charleswang.name>
2020-03-06 02:13:07 +0000

    finished task7_1, cannot execute E01B

--- alu.v
+++ alu.v
@@ -18,34 +18,34 @@ module alu (instruction,
     input carrystatus;        // the Q output from CARRY
     input skipstatus;         // the Q output from SKIP
     
-    output [15:0] aluout;	  // the ALU block output, written into Rd
-    output carryout;	      // the CARRY out, D for CARRY flip flop
-    output skipout;        	// the SKIP output, D for SKIP flip flop
-    output carryen;        	// the enable signal for CARRY flip-flop
-    output skipen;         	// the enable signal for SKIP flip-flop
-    output wenout;         	// the enable for writing Rd in the register file
+    output [15:0] aluout; //  the ALU block output, written into Rd
+    output carryout;      //  the CARRY out, D for CARRY flip flop
+    output skipout;       //  the SKIP output, D for SKIP flip flop
+    output carryen;       //  the enable signal for CARRY flip-flop
+    output skipen;        //  the enable signal for SKIP flip-flop
+    output wenout;        //  the enable for writing Rd in the register file
     
     // these wires are for convenience to make logic easier to see
-    wire [2:0] opinstr 	 = instruction [6:4];	// OP field from IR'
-    wire cwinstr 		 = instruction [7];	// 1 = > write CARRY: CW from IR'
-    wire [3:0] condinstr 	 = instruction [11:8];	// COND field from IR'
-    wire [1:0] cininstr 	 = instruction [13:12];	// CIN field from IR'
-    wire [1:0] code 	 = instruction [15:14];	// bits from IR': must be 11 for ARM instruction
+    wire [2:0] opinstr   = instruction [6:4];	// OP field from IR'
+    wire cwinstr         = instruction [7];	// 1 = > write CARRY: CW from IR'
+    wire [3:0] condinstr = instruction [11:8];	// COND field from IR'
+    wire [1:0] cininstr  = instruction [13:12];	// CIN field from IR'
+    wire [1:0] code      = instruction [15:14];	// bits from IR': must be 11 for ARM instruction
     
     wire arm = &code;
     wire XSR = !opinstr[2] & opinstr[1] & opinstr[0];
     
-    reg [16:0] alusum; 			// the 17 bit sum, 1 extra bit so ALU carry out can be extracted
-    wire cin; 				// The ALU carry input, determined from instruction as in ISA spec
-    wire shiftin; 				// value shifted into bit 15 on XSR, determined as in ISA spec
+    reg [16:0] alusum; //  the 17 bit sum, 1 extra bit so ALU carry out can be extracted
+    wire cin;          //  The ALU carry input, determined from instruction as in ISA spec
+    wire shiftin;      //  value shifted into bit 15 on XSR, determined as in ISA spec
     
-    assign alucout 	 = alusum [16]; 		// carry bit from sum, or shift if OP = 011
-    assign aluout 	 = alusum [15:0]; 	// 16 normal bits from sum
+    assign alucout 	 = alusum [16];   //  carry bit from sum, or shift if OP = 011
+    assign aluout 	 = alusum [15:0]; //  16 normal bits from sum
     
-    assign wenout	 = exec1 & (arm);	// correct timing, to do: add enable condition
-    assign carryen	 = exec1 & cwinstr & (arm);	// correct timing, to do: add enable condition
-    assign carryout	 = XSR ? rsdata[0] : alucout;		// this is correct
-    // note the special case of rsdata[0] when OP = 011
+    assign wenout	 = exec1 & arm;                        //  correct timing, to do: add enable condition
+    assign carryen	 = exec1 & cwinstr & arm;              //  correct timing, to do: add enable condition
+    assign carryout	 = arm & (XSR ? rsdata[0] : alucout);  //  this is correct
+                                                               //  note the special case of rsdata[0] when OP = 011
     assign cin	 = cininstr[1] ? (cininstr[0] ? rsdata[15] : carryout) : (cininstr[0] ? 1 : 0);   		     	//* DONE dummy, to do: replace with correct logic
     assign shiftin 	 = cin;     		// dummy, to do: replace with correct logic
     
@@ -66,7 +66,7 @@ module alu (instruction,
             // to do (optional): add additional instructions as cases here
             // available cases: 3'b100,3'b101,3'b110, 3'b111
             default : alusum = 0;// default output for unimplemented OP values, do not change
-            endcase;
-        end
-        
-        endmodule
+    	endcase;
+    end
+
+endmodule
--- decode.v
+++ decode.v
@@ -1,44 +1,45 @@
-module decode(input FETCH,
-              input EXEC1,
-              input EXEC2,
-              input EQ,
-              input MI,
-              input [15:0] IR,
-              output EXTRA,
-              output Wren,
-              output MUX1,
-              output MUX3,
-              output PC_sload,
-              output PC_cnt_en,
-              output ACC_EN,
-              output ACC_LOAD,
-              output ACC_SHIFTIN,
-              output ADDSUB,
-              output MUX3_useAllBits,
+module decode(input FETCH            ,
+              input EXEC1            ,
+              input EXEC2            ,
+              input EQ               ,
+              input MI               ,
+              input [15:0] IR        ,
+              output EXTRA           ,
+              output Wren            ,
+              output MUX1            ,
+              output MUX3            ,
+              output PC_sload        ,
+              output PC_cnt_en       ,
+              output ACC_EN          ,
+              output ACC_LOAD        ,
+              output ACC_SHIFTIN     ,
+              output ADDSUB          ,
+              output MUX3_useAllBits ,
 	      output P
 );
     // MU0 instruction  
     assign LDA = !IR[15] & !IR[14] & !IR[13] & !IR[12];
     assign STA = !IR[15] & !IR[14] & !IR[13] & IR[12];
-    assign ADD = !IR[15] & !IR[14] & IR[13] & !IR[12];
-    assign SUB = !IR[15] & !IR[14] & IR[13] & IR[12];
-    assign JMP = !IR[15] & IR[14] & !IR[13] & !IR[12];
-    assign JMI = !IR[15] & IR[14] & !IR[13] & IR[12];
-    assign JEQ = !IR[15] & IR[14] & IR[13] & !IR[12];
-    assign STP = !IR[15] & IR[14] & IR[13] & IR[12];
-    assign LDI = IR[15] & !IR[14] & !IR[13] & !IR[12];
-    assign LSR = IR[15] & !IR[14] & IR[13] & !IR[12];
-    assign ASR = IR[15] & !IR[14] & IR[13] & IR[12];
+    assign ADD = !IR[15] & !IR[14] & IR[13]  & !IR[12];
+    assign SUB = !IR[15] & !IR[14] & IR[13]  & IR[12];
+    assign JMP = !IR[15] & IR[14]  & !IR[13] & !IR[12];
+    assign JMI = !IR[15] & IR[14]  & !IR[13] & IR[12];
+    assign JEQ = !IR[15] & IR[14]  & IR[13]  & !IR[12];
+    assign STP = !IR[15] & IR[14]  & IR[13]  & IR[12];
+    assign LDI = IR[15]  & !IR[14] & !IR[13] & !IR[12];
+    assign LSR = IR[15]  & !IR[14] & IR[13]  & !IR[12];
+    assign ASR = IR[15]  & !IR[14] & IR[13]  & IR[12];
    
     // whether is ARM instruction
     wire ARM = IR[15] & IR[14];
     // ARMish opcode    
     assign arm_ADD = ARM & !IR[6] & !IR[5] & !IR[4];
     assign arm_SUB = ARM & !IR[6] & !IR[5] & IR[4];
-    assign arm_MOV = ARM & !IR[6] & IR[5] & !IR[4];
-    assign arm_XSR = ARM & !IR[6] & IR[5] & IR[4];
+    assign arm_MOV = ARM & !IR[6] & IR[5]  & !IR[4];
+    assign arm_XSR = ARM & !IR[6] & IR[5]  & IR[4];
 
-    assign P 		   = LDA | LDI | ADD | SUB | LSR | ASR | JMP | JMI | JEQ;
+    //assign P 		   = LDA | LDI | ADD | SUB | LSR | ASR | JMP | JMI | JEQ;
+    assign P = 0;
 
     assign EXTRA           = LDA & EXEC1 | ADD & EXEC1 | SUB & EXEC1;
     assign Wren            = STA & EXEC1;
